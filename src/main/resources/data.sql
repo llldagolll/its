0dacac7208061339c0bd8bdf5308a8e100f47cc8
nobuya <72905484+llldagolll@users.noreply.github.com>
2023-01-16 13:01:10 +0900

    first commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into issues (summary, description) values ('バグA', 'バグがあります');
+insert into issues (summary, description) values ('機能要望B', '機能を追加してほしい');
+insert into issues (summary, description) values ('画面Cが遅い', '早くしてほしいです');
